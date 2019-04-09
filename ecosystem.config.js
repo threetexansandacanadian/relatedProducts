b5e15bd9d48dce94be6dafd64caee95b3abc0ed9
ryden <ryferr8122@gmail.com>
2019-04-09 11:58:38 -0500

    so much bread

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-219-207-105.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/FEC-ryden.pem',
+      key: '~/.ssh/rsa_id.pem',
       ref: 'origin/master',
       repo: 'https://github.com/threetexansandacanadian/relatedProducts.git',
       path: '/home/ubuntu/FEC',
