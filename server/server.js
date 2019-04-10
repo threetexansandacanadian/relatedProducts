ba2fe0b79be7c80dbb89cade84cca16797a5391c
ryden <ryferr8122@gmail.com>
2019-04-10 14:29:10 -0500

    removed root address in server, using static

--- server/server.js
+++ server/server.js
@@ -5,20 +5,6 @@ const db = require('../database/db');
 
 app.use(express.static('dist'));
 
-// app.get('/', (req, res) => {
-//   res.send('howdy!')
-// })
-
-//route server to db
-// app.get('/api/products', (req, res) => {
-//   //usually the req would have a search term, like * to query the db to get all
-//   db.selectAll((data) => {
-//     console.log('made it db from server!');
-//     //now sending should be the data we got back from the database
-//     res.send(data);
-//   })
-// })
-
 app.get('/api/products', (req, res) => {
   //usually the req would have a search term, like * to query the db to get all
   db.getFifteenEntries((data) => {
