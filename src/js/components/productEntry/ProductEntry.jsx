cfbd0cf4b38d5cd7606567523d23b2f2de4b485f
ryden <ryferr8122@gmail.com>
2019-04-11 14:27:51 -0500

    retrieves thumbnails

--- src/js/components/productEntry/ProductEntry.jsx
+++ src/js/components/productEntry/ProductEntry.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 const ProductEntry = (props) => {
   // const style = {display: 'inline-block'};
+  console.log(props.entry.productpic)
   return (
     <div className="ProductEntry-Container">
-    <div className="ProductEntry-Image">
-      <p>thumbnail</p>
-    </div>
-      <div className="ProductEntry-Name">{props.entry.productName}</div>
-      <div className="ProductEntry-Price">${props.entry.productPrice}</div> 
+  
+      <div className="ProductEntry-Image">
+        <img src={props.entry.productpic}></img>
+      </div>
+
+      <div className="ProductEntry-Name">{props.entry.productname}</div>
+      <div className="ProductEntry-Price">${props.entry.productprice}</div> 
     </div>
   )
 }
